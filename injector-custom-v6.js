fc4a52ee1d3dc72797523c37fc3e879ed2eed822
desginlabo9133-lgtm <desginlabo9133@gmail.com>
2026-03-06 16:30:49 +0900

    Add event listener to modify title and styles on load

---
+++ injector-custom-v6.js
@@ -0,0 +1,6 @@
+window.addEventListener('load', () => {
+  document.title = 'V6 LOADED';
+  document.body.style.background = '#000';
+  document.body.style.outline = '12px solid red';
+  console.log('V6 LOADED');
+});
